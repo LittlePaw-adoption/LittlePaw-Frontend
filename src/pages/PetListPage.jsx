9b79fedb48092c60c97dd949b5b871b7556ac766
AnaRibeiroPT <anaribeiro924@gmail.com>
2023-12-06 16:20:54 +0000

    add PetListPage

--- src/pages/PetListPage.jsx
+++ src/pages/PetListPage.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function PetListPage() {
+  const [petsList, setPetsList] = useState(null);
+
+  const API_URL = import.meta.env.VITE_API_URL
+
+  const storedToken = localStorage.getItem("authToken");
+
+  useEffect(() => {
+    axios
+      .get(API_URL + "/api/pets", { headers: { Authorization: `Bearer ${storedToken}` }})
+      .then((response) => {
+        setPetsList(response.data);
+      })
+      .catch((error) => {
+        console.log("Error getting the list of pets: ", error);
+      });
+  }, []);
+
   return (
-    <></>
-  )
+    <div>
+      {petsList === null ? (
+        <p>Loading...</p>
+      ) : (
+        petsList.map((pets) => {
+          return (
+            <div>
+              <p>Name: {pets.name}</p>
+              <p>Species: {pets.species}</p>
+              <p>Breed: {pets.breed}</p>
+              <p>Age: {pets.age}</p>
+              <p>Description: {pets.description}</p>
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
 }
 
 export default PetListPage;
