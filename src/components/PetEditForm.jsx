100907d520740e0062e3375bf81a51bf3373c86d
Hugo Pereira <hugocastroferro@gmail.com>
2023-12-07 14:38:07 +0000

    [add] API_URL and StoredToken added to missing files

--- src/components/PetEditForm.jsx
+++ src/components/PetEditForm.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+
+const API_URL = import.meta.env.VITE_API_URL;
+const storedToken = localStorage.getItem("authToken");
+
 const PetEditForm = ({ pet, onCancel, onSave }) => {
   const [editedPet, setEditedPet] = useState({ ...pet });
 
