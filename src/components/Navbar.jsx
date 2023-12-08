0c3247313697c02fe367ad13e53d6ac51eca557d
AnaRibeiroPT <anaribeiro924@gmail.com>
2023-12-08 13:23:00 +0000

    improve in homepage css and functionalities

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
-import { useNavigate } from "react-router-dom";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 function Navbar() {
@@ -9,135 +8,140 @@ function Navbar() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <div>
-        <nav className=" top-0 left-0 right-0 z-50 bg-white border-gray-200 dark:bg-gray-900 ">
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <h1 className="flex items-center space-x-3 rtl:space-x-reverse">
-              <img src={logo} className="h-8" alt="LittlePaw Logo" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap text-[#5bc0be]">
-                Little Paw
-              </span>
-            </h1>
-            <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-              {!isLoggedIn && (
+    <nav style={{backgroundColor: "white"}} className="border-gray-200 shadow">
+      <nav className=" top-0 left-0 right-0 z-50 border-gray-200 dark:bg-gray-900 ">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <h1 className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src={logo} className="h-8" alt="LittlePaw Logo" />
+            <NavLink
+              to={"/"}
+              className="self-center text-2xl font-semibold whitespace-nowrap text-[#5bc0be]"
+            >
+              Little Paw
+            </NavLink>
+          </h1>
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            {!isLoggedIn && (
+              <>
+                <button
+                  onClick={() => navigate("/signup")}
+                  type="button"
+                  className="text-white bg-[#5bc0be] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-4"
+                >
+                  Sign up
+                </button>
+                <button
+                  onClick={() => navigate("/login")}
+                  type="button"
+                  className="text-white bg-[#5bc0be] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
+                >
+                  Login
+                </button>
+              </>
+            )}
+            <button
+              data-collapse-toggle="navbar-cta"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-cta"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-cta"
+          >
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <button
+                  onClick={() => navigate("/")}
+                  to="/"
+                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#5bc0be] md:dark:hover:text-[#5bc0be] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 active:text-[#5bc0be]"
+                >
+                  Home
+                </button>
+              </li>
+              {isLoggedIn && (
                 <>
+                  <li>
+                    <NavLink
+                      to="/feed"
+                      className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#5bc0be] md:dark:hover:text-[#5bc0be] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 active:text-[#5bc0be]"
+                    >
+                      Feed
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/pets"
+                      className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#5bc0be] d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 active:text-[#5bc0be]"
+                    >
+                      Pets
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/shelters"
+                      className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#5bc0be] d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 active:text-[#5bc0be]"
+                    >
+                      Shelters{" "}
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/profile"
+                      className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#5bc0be] d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 active:text-[#5bc0be]"
+                    >
+                      Profile
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/pets/create"
+                      className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#5bc0be] d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 active:text-[#5bc0be]"
+                    >
+                      Create Pets
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/shelters/create"
+                      className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#5bc0be] d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 active:text-[#5bc0be]"
+                    >
+                      Create Shelters
+                    </NavLink>
+                  </li>
                   <button
-                    onClick={() => navigate("/signup")}
-                    type="button"
-                    className="text-white bg-[#5bc0be] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-4"
-                  >
-                    Sign up
-                  </button>
-                  <button
-                    onClick={() => navigate("/login")}
-                    type="button"
+                    onClick={logOutUser}
                     className="text-white bg-[#5bc0be] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
                   >
-                    Login
+                    Logout
                   </button>
+                  <span>{user && user.name}</span>
                 </>
               )}
-              <button
-                data-collapse-toggle="navbar-cta"
-                type="button"
-                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                aria-controls="navbar-cta"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 17 14"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M1 1h15M1 7h15M1 13h15"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div
-              className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-              id="navbar-cta"
-            >
-              <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                {isLoggedIn && (
-                  <>
-                    <li>
-                      <NavLink
-                        to="/"
-                        className="block py-2 px-3 md:p-0 text-white bg-[#5bc0be] rounded md:bg-transparent md:text-[#5bc0be] md:dark:text-[#5bc0be]"
-                        aria-current="page"
-                      >
-                        Home
-                      </NavLink>
-                    </li>
-                    <li>
-                      <NavLink
-                        to="/feed"
-                        className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#5bc0be] md:dark:hover:text-[#5bc0be] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                      >
-                        Feed
-                      </NavLink>
-                    </li>
-                    <li>
-                      <NavLink
-                        to="/pets"
-                        className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#5bc0be] d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                      >
-                        Pets
-                      </NavLink>
-                    </li>
-                    <li>
-                      <NavLink to="/shelters" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#5bc0be] d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >
-                        Shelters </NavLink>
-                    </li>
-                    <li>
-                      <NavLink
-                        to="/profile"
-                        className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#5bc0be] d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                      >
-                        Profile
-                      </NavLink>
-                    </li>
-                    <li>
-                      <NavLink
-                        to="/pets/create"
-                        className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#5bc0be] d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                      >
-                        Create Pets
-                      </NavLink>
-                    </li>
-                    <li>
-                      <NavLink
-                        to="/shelters/create"
-                        className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#5bc0be] d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                      >
-                        Create Shelters
-                      </NavLink>
-                    </li>
-                    <button
-                      onClick={logOutUser}
-                      className="text-white bg-[#5bc0be] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
-                    >
-                      Logout
-                    </button>
-                    <span>{user && user.name}</span>
-                  </>
-                )}
-              </ul>
-            </div>
+            </ul>
           </div>
-        </nav>
-      </div>
-    </div>
+        </div>
+      </nav>
+    </nav>
   );
 }
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,16 @@
 import { Link, useNavigate } from "react-router-dom";
-// import Typed from "react-typed"
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import Typed from "react-typed";
 import bgImage from "../assets/bgHero.jpg";
-import adoptPet from "../assets/adoptPet.png"
-import abandonedPet from "../assets/abandonedPet.png"
+import adoptPet from "../assets/adoptPet.png";
+import abandonedPet from "../assets/abandonedPet.png";
 
 function Homepage() {
   const navigate = useNavigate();
+  const [showInfoAlertFeed, setShowInfoAlertFeed] = useState(false);
+  const [showInfoAlertPets, setShowInfoAlertPets] = useState(false);
+  const { isLoggedIn } = useContext(AuthContext);
 
   const containerStyle = {
     position: "relative",
@@ -20,6 +25,24 @@ function Homepage() {
     zIndex: -1,
   };
 
+  const handleClickFeed = () => {
+    if (!isLoggedIn) {
+      setShowInfoAlertFeed(true);
+      setTimeout(() => setShowInfoAlertFeed(false), 4000);
+      return;
+    }
+    navigate("/feed");
+  };
+
+  const handleClickPets = () => {
+    if (!isLoggedIn) {
+      setShowInfoAlertPets(true);
+      setTimeout(() => setShowInfoAlertPets(false), 4000);
+      return;
+    }
+    navigate("/pets");
+  };
+
   return (
     <div>
       <div
@@ -27,74 +50,133 @@ function Homepage() {
         className="text-white relative overflow-hidden"
       >
         <div style={backgroundStyle}></div>
-        <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center pt-60">
+        <div className="max-w-[1000px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center pt-60">
           <p className="text-[#5bc0be] font-bold p-2">
             FIND YOUR FOREVER FRIEND
           </p>
           <h1 className="md:text-6xl sm:text-6xl text-4xl font-bold md:py-6">
-            Adopt a Paw Companion!
+            Adopt a Paw Companion
           </h1>
-          <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
-            Lovable, cute, amazing
-          </p>
+          <div className="flex justify-center items-center">
+            <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
+              Lovable, amazing,
+            </p>
+            <Typed
+              className="md:text-5xl sm:text-4xl text-xl font-bold pl-2 text-[#5bc0be]"
+              strings={["pawesome", "purrfect", "meowvellous", "fur-ever"]}
+              typeSpeed={120}
+              backSpeed={80}
+              loop
+            />
+            <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
+              pets!
+            </p>
+          </div>
           <p className="md:text-2xl text-xl font-bold text-gray-300">
             Find a loyal pet eager to share their love with you!
           </p>
           <button
-            onClick={() => navigate("/feed")}
+            onClick={handleClickFeed}
             className="bg-[#5bc0be] w-[200px] rounded-md font-bold my-6 mx-auto py-3 text-white"
           >
             CHECK FEED
           </button>
+          <div className="flex justify-center">
+          {showInfoAlertFeed && (
+            <div
+              class="flex items-center p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
+              role="alert"
+            >
+              <svg
+                class="flex-shrink-0 inline w-4 h-4 me-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+              </svg>
+              <span class="sr-only">Info</span>
+              <div>
+                <span class="font-medium">Info alert!</span> Cannot access our
+                feed. <b>Login and try again.</b>
+              </div>
+            </div>
+          )}
+          </div>
         </div>
       </div>
       <div className="w-full bg-white py-16 px-4">
-      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
-        <img className="w-[300px] mx-auto my-4" src={adoptPet} alt="/" />
-        <div className="flex flex-col justify-center">
-          <p className="text-[#5bc0be] font-bold">
-            ABOUT US AND OUR LITTLE PAW FRIENDS
-          </p>
-          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
-            Why Choose Little Paw?{" "}
-          </h1>
-          <p className="">
-            At Little Paw, we believe that every paw has a story, and every
-            story deserves a loving home. Adopting a pet is a lifelong
-            commitment, and we're here to support you every step of the way. Our
-            goal is to match you with a furry friend that complements your
-            lifestyle, ensuring a harmonious and loving relationship.
-          </p>
-          <button onClick={() => navigate("/pets")} className="bg-black text-[#5bc0be] w-[200px] rounded-md font-bold my-6 mx-auto md:mx-0 py-3">
-            LOOK FOR A FRIEND
-          </button>
-        </div>
-      </div>
-      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
-        <div className="flex flex-col justify-center">
-          <p className="text-[#5bc0be] font-bold">ANIMAL ABUSE AWARENESS</p>
-          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
-            A Call to Compassion
-          </h1>
-          <p className="">
-            Animal abuse is a harsh reality, causing pain to innocent beings.
-            It's time to open our hearts, recognize this cruelty, and work
-            collectively to ensure a safer, kinder world for our furry friends.
-            Through education, empathy, and collective action, we can create a
-            world where every tail wags with joy. Click below to check more
-            about animal abuse and how to fight it!
-          </p>
-          <div>
-            <Link to="https://care4paws.org/abuse/" target="_blank">
-              <button className="bg-black text-[#5bc0be] w-[300px] rounded-md font-bold my-6 mx-auto md:mx-0 py-3">
-                STOP ANIMAL ABUSE
-              </button>
-            </Link>
+        <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+          <img className="w-[300px] mx-auto my-4" src={adoptPet} alt="/" />
+          <div className="flex flex-col justify-center">
+            <p className="text-[#5bc0be] font-bold">
+              ABOUT US AND OUR LITTLE PAW FRIENDS
+            </p>
+            <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+              Why Choose Little Paw?{" "}
+            </h1>
+            <p className="">
+              At Little Paw, we believe that every paw has a story, and every
+              story deserves a loving home. Adopting a pet is a lifelong
+              commitment, and we're here to support you every step of the way.
+              Our goal is to match you with a furry friend that complements your
+              lifestyle, ensuring a harmonious and loving relationship.
+            </p>
+            <button
+              onClick={handleClickPets}
+              className="bg-black text-[#5bc0be] w-[200px] rounded-md font-bold my-6 mx-auto md:mx-0 py-3"
+            >
+              LOOK FOR A FRIEND
+            </button>
+            {showInfoAlertPets && (
+              <div
+                class="flex items-center p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
+                role="alert"
+              >
+                <svg
+                  class="flex-shrink-0 inline w-4 h-4 me-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                </svg>
+                <span class="sr-only">Info</span>
+                <div>
+                  <span class="font-medium">Info alert!</span> Cannot access our
+                  pets list. <b>Login and try again.</b>
+                </div>
+              </div>
+            )}
           </div>
         </div>
-        <img className="w-[300px] mx-auto my-4" src={abandonedPet} alt="/" />
+        <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+          <div className="flex flex-col justify-center">
+            <p className="text-[#5bc0be] font-bold">ANIMAL ABUSE AWARENESS</p>
+            <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+              A Call to Compassion
+            </h1>
+            <p className="">
+              Animal abuse is a harsh reality, causing pain to innocent beings.
+              It's time to open our hearts, recognize this cruelty, and work
+              collectively to ensure a safer, kinder world for our furry
+              friends. Through education, empathy, and collective action, we can
+              create a world where every tail wags with joy. Click below to
+              check more about animal abuse and how to fight it!
+            </p>
+            <div>
+              <Link to="https://care4paws.org/abuse/" target="_blank">
+                <button className="bg-black text-[#5bc0be] w-[300px] rounded-md font-bold my-6 mx-auto md:mx-0 py-3">
+                  STOP ANIMAL ABUSE
+                </button>
+              </Link>
+            </div>
+          </div>
+          <img className="w-[300px] mx-auto my-4" src={abandonedPet} alt="/" />
+        </div>
       </div>
-    </div>
     </div>
   );
 }
